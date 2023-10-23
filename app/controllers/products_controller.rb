c990950b0134d2d3a1941083d720e03fd6d546ee
Leonardo Lourencini <leolourencini@hotmail.com>
2023-10-23 17:11:16 -0500

    Edited index and show pages to display categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all
+    @categories = Category.all
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-require "faker"
+#require "faker"
+require "csv"
 
 Product.destroy_all
+Category.destroy_all
 
-for prod in 1..676 do
-  prod = Product.create(
-    title: Faker::Commerce.product_name,
-    description: "Best product of the world!",
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.within(range: 1..1000)
+# Populating with Faker
+
+# for prod in 1..676 do
+#   prod = Product.create(
+#     title: Faker::Commerce.product_name,
+#     description: "Best product of the world!",
+#     price: Faker::Commerce.price,
+#     stock_quantity: Faker::Number.within(range: 1..1000)
+#   )
+#   prod.save
+# end
+
+# Populating with CSV
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+
+  next unless category && category.valid?
+
+  category.products.create(
+    title: p["name"],
+    price: p["price"],
+    description: p["description"],
+    stock_quantity: p["stock quantity"]
   )
-  #prod.save
 end
